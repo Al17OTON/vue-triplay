f388b5e30f2837c07a16d155f7899725bc705d9f
임권 <limkwon18@gmail.com>
2024-05-17 00:57:14 +0900

    update: login modal

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -2,47 +2,50 @@
 import { useMemberStore } from "@/stores/memberStore";
 import Swal from "sweetalert2";
 import { Axios } from "@/util/http-commons.js";
+import { ref } from "vue";
 
 const memberStore = useMemberStore();
 const api = Axios();
 
 const login = () => {
-  Swal.fire({
-    title: "로그인",
+ 
+  (async () => {
 
-    html:
-    '<input id="swal-input1" class="swal2-input" placeholder="ID">' +
-    '<input id="swal-input2" class="swal2-input" placeholder="PassWord">',
+    const res = await Swal.fire({
+      title: "로그인",
 
-    inputAttributes: {
-      autocapitalize: "off"
-    },
-    showCancelButton: true,
-    confirmButtonText: "login",
-    showLoaderOnConfirm: true,
-    preConfirm: async () => {
-      try {
-        api.post("login", {
-          member_id: id,
-          member_pw: password
-        })
-        .then((res) => console.log(res));
+      html:
+      '<input id="swal-input1" class="swal2-input" placeholder="ID">' +
+      '<input id="swal-input2" class="swal2-input" placeholder="Password">',
+
+      inputAttributes: {
+        autocapitalize: "off",
+        input: 'text',
+        required: 'true'
+      },
+      showCancelButton: true,
+      confirmButtonText: "login",
+      showLoaderOnConfirm: true,
+      
+      preConfirm: async () => {
+        const id = document.getElementById('swal-input1').value;
+        const pw = document.getElementById('swal-input2').value;
         
-      } catch (error) {
-        Swal.showValidationMessage(`
-          Request failed: ${error}
-        `);
+        if(!id || !pw) Swal.showValidationMessage("아이디와 비밀번호를 모두 입력해주세요.");     
+        
+          const response = await api.post("member/login", {
+              member_id : id,
+              member_pw : pw
+          }).then(res => res.json());
+          // if(!response.ok) {
+          //   return Swal.showValidationMessage("로그인 정보가 올바르지 않습니다."); 
+          // }
+          return response;
       }
-    },
-    allowOutsideClick: () => !Swal.isLoading()
-  }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire({
-        title: `${result.value.login}'s avatar`,
-        imageUrl: result.value.avatar_url
-      });
-    }
-  });
+    });
+
+    
+  })();
 }
 
 </script>
